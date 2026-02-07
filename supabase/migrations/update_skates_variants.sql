135801fafc011e6b7228750c72bf13f5e613cb3c
Angel Santana <angellafraga@gmail.com>
2026-02-07 15:11:40 -0400

    Migration: auto-assign size variants to all skate products

---
+++ supabase/migrations/update_skates_variants.sql
@@ -0,0 +1,6 @@
+﻿-- Update all products in 'patines' category to have size variants
+UPDATE skating_products
+SET 
+  variant_type = 'size',
+  variant_options = ARRAY['36', '37', '38', '39', '40', '41', '42', '43', '44', '45']
+WHERE category = 'patines';
